a55b2ac8323a081116eedb77c83f13349c5b858e
kilik22 <126161330+kilik22@users.noreply.github.com>
2023-05-30 08:45:15 +0200

    Add files via upload

---
+++ foder/j.js
@@ -0,0 +1,29 @@
+$('.carousel').carousel()
+
+let itemsCart = [];
+let totalPrice = 0;
+
+function myFunction() {
+    console.log(document.getElementById('g1').innerText);
+    console.log(document.getElementById('g1.1').innerText);
+}
+function myFunction2() {
+    console.log(document.getElementById('g2').innerText);
+    console.log(document.getElementById('g2.2').innerText);
+}
+function myFunction3() {
+    console.log(document.getElementById('g3').innerText);
+    console.log(document.getElementById('g3.3').innerText);
+}
+function myFunction4() {
+    console.log(document.getElementById('g4').innerText);
+    console.log(document.getElementById('g4.4').innerText);
+}
+function myFunction5() {
+    console.log(document.getElementById('g5').innerText);
+    console.log(document.getElementById('g5.5').innerText);
+}
+function myFunction6() {
+    console.log(document.getElementById('g6').innerText);
+    console.log(document.getElementById('g6.6').innerText);
+}
